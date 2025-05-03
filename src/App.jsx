55ae6273837fbcb8a7b38d7ca8fcb4b6a2781c7a
Shubham140 <Shubham140@gmail.com>
2025-05-04 02:21:29 +0530

    fixed router issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      document
+        .getElementById("main")
+        ?.scrollIntoView({ top: -10, behavior: "instant" });
+    }, 0);
+  }, [location]);
+
   return (
     <div style={{ margin: "-8px" }}>
       <Navbar />
       <Outlet />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
